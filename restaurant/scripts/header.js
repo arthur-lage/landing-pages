f55f2be553bb0d1a81651ddfd5986b454138e344
Arthur Lage <arthurlage2006@gmail.com>
2021-12-18 15:58:02 -0300

    more responsivity

--- restaurant/scripts/header.js
+++ restaurant/scripts/header.js
@@ -9,11 +9,13 @@ mobileMenu.addEventListener("click", () => {
     mobileMenu.classList.toggle("open")
 
     if(mobileMenu.classList.contains("open")) {
-        document.body.classList.add("menuIsOpen")
         const header = document.querySelector("header")
+        
+        document.body.classList.add("menuIsOpen")
         header.classList.add("sticky")
     } else {
-        document.body.classList.add("menuIsOpen")
+        const header = document.querySelector("header")
+
         document.body.classList.remove("menuIsOpen")
         header.classList.remove("sticky")
     }
