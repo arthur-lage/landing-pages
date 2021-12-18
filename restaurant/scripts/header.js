9a81d954561bea9f6ef72036506d1c8d63910e57
Arthur Lage <arthurlage2006@gmail.com>
2021-12-17 22:54:18 -0300

    started responsibility

--- restaurant/scripts/header.js
+++ restaurant/scripts/header.js
@@ -2,3 +2,19 @@ window.addEventListener("scroll", () => {
     const header = document.querySelector("header")
     header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+const mobileMenu = document.querySelector(".mobile-menu")
+
+mobileMenu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open")
+
+    if(mobileMenu.classList.contains("open")) {
+        document.body.classList.add("menuIsOpen")
+        const header = document.querySelector("header")
+        header.classList.add("sticky")
+    } else {
+        document.body.classList.add("menuIsOpen")
+        document.body.classList.remove("menuIsOpen")
+        header.classList.remove("sticky")
+    }
+})
